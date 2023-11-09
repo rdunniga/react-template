24f732de0d7d71930310acdb2150ef0cae3d22be
Ramon Dunnigan <rdunniga@riversidesheriff.org>
2023-11-09 06:34:40 -0800

    refactored logic

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,17 +1,10 @@
-import React, { useContext, useEffect }  from 'react';
-import { UserContext } from '../context/UserContext';
+import React from 'react';
 
 function About() {
-  const { userId, setUserId } = useContext(UserContext);
-
-  useEffect(() => {
-    console.log('about userId', userId);
-  });
 
   return (
     <div className="about">
       <h1>About</h1>
-      <h2>{userId}</h2>
     </div>
   )
 }
